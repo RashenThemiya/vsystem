2b988289aa70bad9841944309400933518eebead
Rehan OP <rehanop007@gmail.com>
2026-01-21 22:39:26 +0530

    add vehicle update

--- Frontend/src/pages/AdminDashboard/StatsCards.jsx
+++ Frontend/src/pages/AdminDashboard/StatsCards.jsx
@@ -15,7 +15,7 @@ export default function StatsCards({
   onSelectPendingBills,
 }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
       
       {/* TOTAL TRIPS */}
       <Card
